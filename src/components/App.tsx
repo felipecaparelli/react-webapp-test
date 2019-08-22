5f0f695f6cc4975713fef1765fbb73fd217b7a27
Felipe Caparelli <felipecaparelli@gmail.com>
2019-08-22 17:56:00 +0200

    libraried added

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,24 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import styled, { ThemeProvider } from 'styled-components';
+
+// Define what props.theme will look like
+const theme = {
+  main: "royalblue"
+};
+
+const Button = styled.button`
+  cursor: pointer;
+  background: transparent;
+  font-size: 16px;
+  border-radius: 10px;
+  color: ${props => props.theme.main};
+  border: 2px solid ${props => props.theme.main};
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  transition: 0.5s all ease-out;
+
+  &:hover {
+    background-color: ${props => props.theme.main};
+    color: white;
+  }
+`;
+
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Button>Open App</Button>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
